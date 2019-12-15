2aca75fa409312ea82dbaf9778dfd16d9af05b94
Miles Marillo <mjm03@hampshire.edu>
2019-12-15 10:46:10 -0500

    render json works

--- app/controllers/api/v1/artworks_controller.rb
+++ app/controllers/api/v1/artworks_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::ArtworksController < ApplicationController
+
+def index
+    artworks = Artwork.all
+    render json: artworks
+end
+
+def show
+    artwork = Artwork.find(:index)
+    render json: artwork
+end
+
+def create
+    artwork = Artwork.create!(artwork_params)
+end
+
+def destroy
+    artwork.destroy
+end
+
 end
--- app/controllers/api/v1/museums_controller.rb
+++ app/controllers/api/v1/museums_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::MuseumsController < ApplicationController
-end
+
+    def index
+        museums = Museum.all
+        render json: museums
+    end
+    
+    def show
+        museum = Museum.find(:index)
+        render json: museum
+    end
+    
+    def create
+        museum = Museum.create!(museum_params)
+    end
+    
+    def destroy
+        museum.destroy
+    end
+    
+    end
+    
